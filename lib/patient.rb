e746bc035efa7e604aca4525a756940f2223fecb
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 18:10:48 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, doctor)
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
